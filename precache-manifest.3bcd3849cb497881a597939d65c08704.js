552e6ed1fe564f502311b0baca638b06b3802312
Abdullah Safi <asaf@uni.sydney.edu.au>
2021-04-29 16:02:21 +1000

    Updates

---
+++ precache-manifest.3bcd3849cb497881a597939d65c08704.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac6731056a16bf0a625d673efb836e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bf5fb41d84f7c667f32",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf5fb41d84f7c667f32",
+    "url": "/static/js/main.42a5bc53.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "1a56fa2a9c12ec564e0c2e4438d780e6",
+    "url": "/static/media/headshot.1a56fa2a.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
