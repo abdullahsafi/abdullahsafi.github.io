13389c9c39bc708f717710bfbe5e70ce5b7265a5
abdullahsafi <abdullah.safi@outlook.com.au>
2022-01-15 20:02:50 +1100

    Updates

---
+++ precache-manifest.8a600e2559b63527d1fbaaadcd452020.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf57edfce5ab9ddf9d4a2f870b3d1702",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55cd9968313be945cd30",
+    "url": "/static/css/main.b5792bd0.chunk.css"
+  },
+  {
+    "revision": "a1ab01db790be7f92755",
+    "url": "/static/js/2.e36958a6.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.e36958a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55cd9968313be945cd30",
+    "url": "/static/js/main.4268702e.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "1a56fa2a9c12ec564e0c2e4438d780e6",
+    "url": "/static/media/headshot.1a56fa2a.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
