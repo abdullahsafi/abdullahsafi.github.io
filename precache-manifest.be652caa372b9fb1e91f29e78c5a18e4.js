0b72a259b44c8c1387d6a1a642fbf302bf07ec8b
abdullahsafi <abdullah.safi@outlook.com.au>
2022-02-05 15:23:53 +1100

    Updates

---
+++ precache-manifest.be652caa372b9fb1e91f29e78c5a18e4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e370c9fd39a3f94cf10fcf968d55c9a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "190a2456c15243358cf8",
+    "url": "/static/css/main.b5792bd0.chunk.css"
+  },
+  {
+    "revision": "32c1cc5b11961331a719",
+    "url": "/static/js/2.ac2d3643.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.ac2d3643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "190a2456c15243358cf8",
+    "url": "/static/js/main.aa95f528.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "1a56fa2a9c12ec564e0c2e4438d780e6",
+    "url": "/static/media/headshot.1a56fa2a.png"
+  },
+  {
+    "revision": "fb4f4a5050d57c4d7bf70acff53c5a8f",
+    "url": "/static/media/headshot3.fb4f4a50.jpg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
